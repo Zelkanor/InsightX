072497136191627220945415afa648cfe72bc33e
Nikhil Krishna <nikhil.krishnakn@gmail.com>
2026-02-10 20:13:38 +0530

    Implement mongodb

---
+++ src/database/mongoose.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+const cached = global.mongooseCache;
+
+if (!cached) {
+  global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
+  }
+
+  if (!cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    console.debug("Connected to MongoDB");
+  }
+};
